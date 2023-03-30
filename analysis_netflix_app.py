9c7416b321e9c4e9dad3ce873d7204e5032d191d
luizaalovo <94422867+luizaalovo@users.noreply.github.com>
2023-03-30 15:00:46 -0300

    :sparkles: add: inicio app streamlit

---
+++ analysis_netflix_app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write("# Analysis Netflix")
+st.write("Netflix é uma das plataformas de streaming de mídia e vídeo mais populares. Este conjunto de dados tabulares consiste em listas de todos os filmes e séries disponíveis na Netflix, juntamente com detalhes como elenco, diretores, classificações, ano de lançamento, duração, etc.")
